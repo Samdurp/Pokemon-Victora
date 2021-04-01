74cbd56175e792dfa112a7eb90d69037c25fafc3
Noah Lang <noahpparentreason@gmail.com>
2021-04-01 17:52:36 -0400

    See description for changes
    - Fixed some trees that didn't have collision
    - Snaps player to nearest grid position in Start(), so it can be moved around in the editor non-precisely.
    - Added grass collision
    - Added wild Pokemon encounter "calculator" function. Battles not yet implemented.

--- Pekoman Victory/Assets/Global Scripts/PlayerMovement.cs
+++ Pekoman Victory/Assets/Global Scripts/PlayerMovement.cs
@@ -6,15 +6,40 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public LayerMask collisionLayer;
+    public LayerMask grassLayer;
     public Camera camera;
+    public bool showDot;
+    public Sprite dotSprite;
 
     private bool isMoving;
-
+    private SpriteRenderer dot;
     private Vector3 input;
 
     private void Start()
     {
+        // Dot that shows the target position inputted by the player
+        dot = GameObject.Find("Dot").GetComponent<SpriteRenderer>();
+
+        if (showDot == false)
+            dot.enabled = false;
+
+        // These basically make sure the player spawns on-grid, even if placed off-grid in the editor
+        if (transform.position.x >= 0 && transform.position.y >= 0)
+            transform.position = new Vector2(((int)transform.position.x) + .5f, ((int)transform.position.y) + .75f);
+
+        else if (transform.position.x >= 0 && transform.position.y < 0)
+            transform.position = new Vector2(((int)transform.position.x) + .5f, ((int)transform.position.y) - .25f);
+
+        else if (transform.position.x < 0 && transform.position.y < 0)
+            transform.position = new Vector2(((int)transform.position.x) - .5f, ((int)transform.position.y) - .25f);
+
+        else if (transform.position.x < 0 && transform.position.y >= 0)
+            transform.position = new Vector2(((int)transform.position.x) - .5f, ((int)transform.position.y) + .75f);
+
+        // Align the camera and dot with the player 
         camera.transform.position = new Vector3(transform.position.x, transform.position.y, -100);
+        dot.transform.position = new Vector2(transform.position.x, transform.position.y);
+
     }
 
     private void Update()
@@ -24,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            // Removes diagonal movement
             if (input.x != 0)
                 input.y = 0;
 
@@ -32,6 +58,9 @@ public class PlayerMovement : MonoBehaviour
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
+                dot.transform.position = new Vector2(targetPos.x, targetPos.y - .25f);
+
+                EncounterPokemon();
 
                 if (IsWalkable(targetPos))
                     StartCoroutine(Move(targetPos));
@@ -56,5 +85,19 @@ public class PlayerMovement : MonoBehaviour
         if (Physics2D.OverlapCircle(targetPosition, .1f, collisionLayer) != null)
             return false;
         return true;
+    }
+
+    private bool EncounterPokemon()
+    {
+        if (Physics2D.OverlapCircle(transform.position, .3f, grassLayer) != null)
+        {
+            int randomNum = Random.Range(1, 20);
+            if (randomNum == 1)
+            {
+                Debug.Log("Pokemon encountered");
+                return true;
+            }
+        }
+        return false;
     }
 }

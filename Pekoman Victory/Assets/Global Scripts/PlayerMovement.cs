fb5c6160215367eedc4d5c6e9323be43f3876a9e
Noah Lang <noahpparentreason@gmail.com>
2021-04-01 02:13:48 -0400

    Read description
    - Added menu screen
    - Added character
    - Added character movement
    - Added environment art
    - Added environment collision

---
+++ Pekoman Victory/Assets/Global Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public LayerMask collisionLayer;
+    public Camera camera;
+
+    private bool isMoving;
+
+    private Vector3 input;
+
+    private void Start()
+    {
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, -100);
+    }
+
+    private void Update()
+    {
+        if (isMoving == false)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0)
+                input.y = 0;
+
+            if (input!= Vector3.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
+            }
+        }
+
+        IEnumerator Move(Vector3 targetPosition)
+        {
+            isMoving = true;
+            while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+                camera.transform.position = Vector3.MoveTowards(camera.transform.position, new Vector3(targetPosition.x, targetPosition.y, -100), movementSpeed * Time.deltaTime);
+                yield return null;
+            }
+            isMoving = false;
+        }
+    }
+
+    private bool IsWalkable(Vector3 targetPosition)
+    {
+        if (Physics2D.OverlapCircle(targetPosition, .3f, collisionLayer) != null)
+            return false;
+        return true;
+    }
+}
---
+++ Pekoman Victory/Assets/Menu/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void PlayButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

58be3397e6933c7fa6f1b1ebb95fae7d71303cb9
Noah Lang <noahpparentreason@gmail.com>
2021-04-01 23:40:26 -0400

    Added sprite sheets

---
+++ Pekoman Victory/Assets/Pokemon/PokemonBase.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new Pokemon")]
+public class PokemonBase : ScriptableObject
+{
+    //Name
+    [SerializeField] string name;
+
+    // Gender of the Pokemon
+    [SerializeField] PokemonGender gender;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    // Pokedex Number
+    [SerializeField] int dexNo;
+
+    // Sprites
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    // Types 
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    // Stats
+    [SerializeField] int maxHP;
+    [SerializeField] int attack;
+    [SerializeField] int specialAttack;
+    [SerializeField] int defense;
+    [SerializeField] int specialDefense;
+    [SerializeField] int speed;
+
+    // CatchRate
+    [SerializeField] int catchRate;
+
+    // This does not reflect my views; there are more than 2 genders irl. But not for Pokemon!
+    public enum PokemonGender
+    {
+        Male, Female
+    }
+
+    public enum PokemonType
+    {
+        None, 
+        Normal,
+        Fire,
+        Water,
+        Electric,
+        Grass,
+        Ice,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Psychic,
+        Bug,
+        Rock,
+        Ghost,
+        Dark,
+        Dragon,
+        Steel,
+        Fairy
+    }
+}
